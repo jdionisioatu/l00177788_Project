1cbf71e45dd8d985f33cfeeaa5e5465eb3987915
Jorge Dionisio <L00177788@atu.ie>
2024-04-21 21:51:43 +0100

    Update to Launch Controller

--- CodeScanning/Controllers/LaunchScan.cs
+++ CodeScanning/Controllers/LaunchScan.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Formats.Tar;
 
 
 namespace CodeScanning.Controllers
@@ -47,6 +48,60 @@ namespace CodeScanning.Controllers
         public IActionResult IndexWithPage(int pagenumber)
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Scan(int Id, string Name)
+        {
+            var settings = _context.Settings.FirstOrDefault();
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private static Stream CreateTarballForDockerfileDirectory(string directory)
+        {
+            var tarball = new MemoryStream();
+            var files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            using var archive = new TarOutputStream(tarball)
+            {
+                //Prevent the TarOutputStream from closing the underlying memory stream when done
+                IsStreamOwner = false
+            };
+
+            foreach (var file in files)
+            {
+                //Replacing slashes as KyleGobel suggested and removing leading /
+                string tarName = file.Substring(directory.Length).Replace('\\', '/').TrimStart('/');
+
+                //Let's create the entry header
+                var entry = TarEntry.CreateTarEntry(tarName);
+                using var fileStream = File.OpenRead(file);
+                entry.Size = fileStream.Length;
+                entry.TarHeader.Mode = Convert.ToInt32("100755", 8); //chmod 755
+                archive.PutNextEntry(entry);
+
+                //Now write the bytes of data
+                byte[] localBuffer = new byte[32 * 1024];
+                while (true)
+                {
+                    int numRead = fileStream.Read(localBuffer, 0, localBuffer.Length);
+                    if (numRead <= 0)
+                        break;
+
+                    archive.Write(localBuffer, 0, numRead);
+                }
+
+                //Nothing more to do with this entry
+                archive.CloseEntry();
+            }
+            archive.Close();
+
+            //Reset the stream and return it, so it can be used by the caller
+            tarball.Position = 0;
+            return tarball;
         }
     }
 }
